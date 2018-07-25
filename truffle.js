55265d9bc7a15348f952e43cb7bea5a0c38961a1
nflaig <nflaig@protonmail.com>
2018-07-25 16:25:24 +0200

    Add Ropsten network to config

--- truffle.js
+++ truffle.js
@@ -5,6 +5,16 @@ module.exports = {
       port: 8545,
       gas: 6500000,
       network_id: "*"
+    },
+    ropsten: {
+        provider: function() {
+            var HDWalletProvider = require("truffle-hdwallet-provider");
+            var infura_api_key = "1fb318e1115d4e43a6a33ea4a9236514";
+            var mnemonic = "<REPLACE THIS WITH YOUR METAMASK SEED WORDS>";
+            return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/" + infura_api_key);
+        },
+        gas: 4500000,
+        network_id: '3'
     }
   },
   solc: {
